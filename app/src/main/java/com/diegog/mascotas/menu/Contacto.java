d3c9e0a250babae741f96948071cb94281be953b
Diego Guadarrama <flogog@localhost.localdomain>
2016-06-20 01:29:21 -0400

    Add Email

--- app/src/main/java/com/diegog/mascotas/menu/Contacto.java
+++ app/src/main/java/com/diegog/mascotas/menu/Contacto.java
@@ -5,10 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.diegog.mascotas.R;
 
 public class Contacto extends AppCompatActivity {
+
+    private EditText etCorreo;
+    private EditText etNombre;
+    private EditText etMensaje;
+    private Button botonEnviar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +34,21 @@ public class Contacto extends AppCompatActivity {
                 NavUtils.navigateUpTo(Contacto.this, Contacto.this.getIntent());
             }
         });
+
+        etCorreo = (EditText) findViewById(R.id.etCorreo);
+        etNombre = (EditText) findViewById(R.id.etNombre);
+        etMensaje = (EditText) findViewById(R.id.etMensaje);
+
+        botonEnviar = (Button) findViewById(R.id.etBoton);
+
+        botonEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Contacto.this,"Mensaje Enviado",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
+    
 }
